dc7834130baf0a7ad71af20fb7f5aefa21c7b68c
Wyatt Davis <wyattjoe40@live.com>
2020-09-02 20:37:48 -0400

    Further styles the tab menu

--- src/TabMenu.js
+++ src/TabMenu.js
@@ -9,23 +9,36 @@ class TabMenu extends Component {
     }
 
     this.onTabSelected = this.onTabSelected.bind(this)
+    this.isSelected = this.isSelected.bind(this)
   }
 
   onTabSelected(e) {
+    e.preventDefault()
     this.setState({ selectedTab: e.target.innerText })
   }
 
+  isSelected(child) {
+    return child.props.title === this.state.selectedTab
+  }
+
   render() {
+    const allTabItemStyle = "rounded-t-md tab-menu-item border "
+    const selectedTabItemStyle = allTabItemStyle + "tab-item-selected"
+    const unselectedTabItemStyle = allTabItemStyle + "tab-item-unselected border-transparent hover:border-gray-300"
     return (
       <div>
         { this.props.title && 
         <h1>{this.props.title}</h1> }
-        <ul className="flex">
+        <ul className="tab-menu w-full flex flex-wrap border-b">
           {this.props.children.map((child) => (
-            <li className="p-2"><button className="p-1 bg-green-500" onClick={this.onTabSelected}>{child.props.title}</button></li>
+            <li className={`${this.isSelected(child) ? selectedTabItemStyle : unselectedTabItemStyle }`}>
+              <button onClick={this.onTabSelected}  className="px-4 py-2 outline-none">{child.props.title}</button>
+            </li>
           ))}
         </ul>
-        { this.props.children.filter((child) => child.props.title === this.state.selectedTab) }
+        <div className="mt-2">
+          { this.props.children.filter((child) => this.isSelected(child)) }
+        </div>
       </div>
     );
   }
